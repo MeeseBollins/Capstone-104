cf1e41ff389b6572f7772d3f57a893b2e366e7b8
Reese Collins <258563928+MeeseBollins@users.noreply.github.com>
2026-03-11 17:05:04 -0700

    Create TemperatureProbe.ino

---
+++ TemperatureProbe.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme; // I2C
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BME280 test"));
+
+  // Default address is usually 0x77 or 0x76
+  if (!bme.begin(0x76)) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  delay(2000);
+} 
+
+// Add temperature code
